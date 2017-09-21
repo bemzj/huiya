43a70c70cbfa82ee69932ad9ac6c88f4c540c0bf
cyh <385323977@qq.com>
2017-09-21 12:05:59 +0800

    9211205

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+var scrollh;
 //轮播图
 var banner = new Swiper('.swiper-container-banner', {
     direction: 'horizontal',
@@ -56,12 +57,28 @@ $(function(){
 		$(this).find('.twoNav').stop().slideUp();
 		$(this).children('a').css('color','#333333');
 	});
+	//弹出
+	var scrollhh;
+	$('.know a').click(function(){
+		scrollhh = scrollh;
+		$('#content').stop().slideUp(400);
+		$('#popWindow').stop().animate({top:'0%'},400);
+	});
+	$('#popWindow').click(function(){		
+		$('#content').stop().slideDown(400,function(){
+			$(document).scrollTop(scrollhh);
+			$('#popWindow').stop().animate({top:'100%'},400);
+			$('#popWindow').scrollTop(0);
+		});	
+	});
 });
 //窗口大小改变时
 $(window).resize(function(){
 	$('#video .videoplay').css('padding-top',($('#video').height()-$('#video .videoplay').height())/2);
 });
 $(window).scroll(function(){
+	//记录滚动条位置
+	scrollh = $(document).scrollTop();
 	//视频播放按钮垂直居中
 	$('#video .videoplay').css('padding-top',($('#video').height()-$('#video .videoplay').height())/2);
 });
